791328e1ffb746b47c62686e8d7f072cecb7ab44
Vincenzo Maffione <v.maffione@gmail.com>
2016-01-02 11:26:23 +0200

    rina_shim_eth_fa_req: sketch workflow

--- kernel/rina-shim-eth.c
+++ kernel/rina-shim-eth.c
@@ -30,6 +30,7 @@
 #include <linux/list.h>
 #include <linux/wait.h>
 #include <linux/sched.h>
+#include <linux/netdevice.h>
 
 
 #define ETH_P_RINA  0xD1F0
@@ -37,15 +38,16 @@
 struct arpt_entry {
     /* Targed Hardware Address. */
     uint8_t tha[6];
-    /* Targed Protocol Address. */
-    struct rina_name tpa;
+
+    /* Targed Protocol Address is flow->remote_application. */
+    struct flow_entry *flow;
 
     struct list_head node;
 };
 
 struct rina_shim_eth {
     struct ipcp_entry *ipcp;
-
+    struct net_device *netdev;
     struct list_head arp_table;
 };
 
@@ -60,6 +62,7 @@ rina_shim_eth_create(struct ipcp_entry *ipcp)
     }
 
     priv->ipcp = ipcp;
+    priv->netdev = NULL;
     INIT_LIST_HEAD(&priv->arp_table);
 
     printk("%s: New IPC created [%p]\n", __func__, priv);
@@ -75,8 +78,11 @@ rina_shim_eth_destroy(struct ipcp_entry *ipcp)
 
     list_for_each_entry_safe(entry, tmp, &priv->arp_table, node) {
         list_del(&entry->node);
-        rina_name_free(&entry->tpa);
         kfree(entry);
+    }
+
+    if (priv->netdev) {
+        dev_put(priv->netdev);
     }
 
     kfree(priv);
@@ -90,11 +96,18 @@ arp_lookup_direct(struct rina_shim_eth *priv, const struct rina_name *dst_app)
     struct arpt_entry *entry;
 
     list_for_each_entry(entry, &priv->arp_table, node) {
-        if (rina_name_cmp(&entry->tpa, dst_app) == 0) {
+        if (rina_name_cmp(&entry->flow->remote_application, dst_app) == 0) {
             return entry;
         }
     }
 
+    return NULL;
+}
+
+static struct sk_buff *
+arp_request_create(struct rina_shim_eth *priv,
+                   struct flow_entry *flow)
+{
     return NULL;
 }
 
@@ -103,13 +116,35 @@ rina_shim_eth_fa_req(struct ipcp_entry *ipcp,
                      struct flow_entry *flow)
 {
     struct rina_shim_eth *priv = ipcp->priv;
-    struct arpt_entry *entry = arp_lookup_direct(priv, &flow->remote_application);
+    struct arpt_entry *entry;
+    struct sk_buff *skb;
 
-    if (!entry) {
-        return -EINVAL;
+    if (!priv->netdev) {
+        return -ENXIO;
     }
 
-    return -EINVAL;
+    entry = arp_lookup_direct(priv, &flow->remote_application);
+    if (entry) {
+        return -EBUSY;
+    }
+
+    entry = kmalloc(sizeof(*entry), GFP_KERNEL);
+    if (!entry) {
+        PE("%s: Out of memory\n", __func__);
+        return -ENOMEM;
+    }
+
+    entry->flow = flow; /* XXX flow_get() ? */
+    list_add_tail(&entry->node, &priv->arp_table);
+
+    skb = arp_request_create(priv, flow);
+    if (!skb) {
+        return -ENOMEM;
+    }
+
+    dev_queue_xmit(skb);
+
+    return 0;
 }
 
 static int
@@ -142,7 +177,17 @@ rina_shim_eth_config(struct ipcp_entry *ipcp,
     struct rina_shim_eth *priv = (struct rina_shim_eth *)ipcp->priv;
     int ret = -EINVAL;
 
-    (void)priv;
+    if (strcmp(param_name, "netdev") == 0) {
+        void *ns = &init_net;
+#ifdef CONFIG_NET_NS
+        ns = current->nsproxy->net_ns;
+#endif
+        priv->netdev = dev_get_by_name(ns, param_value);
+        if (priv->netdev) {
+            ret = 0;
+            PD("%s: netdev set to %p\n", __func__, priv->netdev);
+        }
+    }
 
     return ret;
 }
