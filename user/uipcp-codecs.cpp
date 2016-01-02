4a2aefed1794a6cf25966f3d2eb0adb454ebf1c2
Vincenzo Maffione <v.maffione@gmail.com>
2016-01-02 11:26:23 +0200

    uipcp-rib: added codec for gpb::enrollmentInformation_t

--- user/uipcp-codecs.cpp
+++ user/uipcp-codecs.cpp
@@ -1,5 +1,53 @@
 #include <iostream>
+#include <list>
+#include <string>
+
+#include "rinalite/rinalite-common.h"
 
 #include "uipcp-codecs.hpp"
 
+#include "EnrollmentInformationMessage.pb.h"
+
 using namespace std;
+
+
+EnrollmentInfo::EnrollmentInfo(const char *buf, unsigned int size)
+{
+    gpb::enrollmentInformation_t gm;
+
+    gm.ParseFromArray(buf, size);
+
+    address = gm.address();
+#if 0
+    start_early = gm.startearly();
+#else
+    start_early = true;
+#endif
+
+    for (int i = 0; i < gm.supportingdifs_size(); i++) {
+        lower_difs.push_back(gm.supportingdifs(i));
+    }
+}
+
+unsigned int EnrollmentInfo::serialize(char *buf, unsigned int size) const
+{
+    gpb::enrollmentInformation_t gm;
+
+    gm.set_address(address);
+    gm.set_startearly(start_early);
+
+    for (list<string>::const_iterator dif = lower_difs.begin();
+                            dif != lower_difs.end(); dif++) {
+        gm.add_supportingdifs(*dif);
+    }
+
+    if (gm.ByteSize() > size) {
+        PE("%s: User buffer too small [%u/%u]\n", __func__,
+                gm.ByteSize(), size);
+        return -1;
+    }
+
+    gm.SerializeToArray(buf, size);
+
+    return gm.ByteSize();
+}
--- user/uipcp-codecs.hpp
+++ user/uipcp-codecs.hpp
@@ -1,4 +1,18 @@
 #ifndef __UIPCP_CODECS_H__
 #define __UIPCP_CODECS_H__
 
+#include <stdint.h>
+#include <list>
+#include <string>
+
+struct EnrollmentInfo {
+    uint64_t address;
+    std::list< std::string > lower_difs;
+    bool start_early;
+
+    EnrollmentInfo() : address(0), start_early(false) { }
+    EnrollmentInfo(const char *buf, unsigned int size);
+    unsigned int serialize(char *buf, unsigned int size) const;
+};
+
 #endif  /* __UIPCP_CODECS_H__ */
