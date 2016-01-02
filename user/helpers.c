fd2734acc03fc3fe849d1bbc9fae1208c2d99cc2
Vincenzo Maffione <v.maffione@gmail.com>
2016-01-02 11:26:23 +0200

    forgot helpers

---
+++ user/helpers.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#include <rina/rina-utils.h>
+#include <rina/rina-application-msg.h>
+
+
+int rina_msg_write(int sfd, struct rina_msg_base *msg)
+{
+    unsigned int serlen;
+    char *serbuf;
+    int n;
+
+    serlen = rina_msg_serlen(rina_application_numtables, msg);
+    serbuf = malloc(serlen);
+    if (!serbuf) {
+        return -1;
+    }
+
+    serialize_rina_msg(rina_application_numtables, serbuf, msg);
+
+    n = write(sfd, serbuf, serlen);
+    if (n != serlen) {
+        printf("write failed [%d/%d]\n", n, serlen);
+    }
+
+    free(serbuf);
+
+    return (n == serlen) ? 0 : -1;
+}
+
---
+++ user/helpers.h
@@ -0,0 +1,6 @@
+#ifndef __RINA_HELPERS_H__
+#define __RINA_HELPERS_H__
+
+int rina_msg_write(int sfd, struct rina_msg_base *msg);
+
+#endif  /* __RINA_HELPERS_H__ */
