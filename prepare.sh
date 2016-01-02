946c8737dbec2ca44ddcb37e7e0a2ea7be2528c6
Vincenzo Maffione <v.maffione@gmail.com>
2016-01-02 11:26:23 +0200

    [un]prepare.sh: integrate VMPI-KVM

--- prepare.sh
+++ prepare.sh
@@ -2,6 +2,13 @@
 
 set -x
 
+# prepare VMPI-KVM
+pushd .
+cd ../vmpi
+./prepare-host-kvm.sh
+./prepare-guest-kvm.sh
+popd
+
 sudo insmod kernel/rina-ctrl.ko
 sudo insmod kernel/rina-shim-dummy.ko
 sudo insmod kernel/rina-shim-hv.ko
--- unprepare.sh
+++ unprepare.sh
@@ -5,3 +5,10 @@ set -x
 sudo rmmod rina-shim-dummy.ko
 sudo rmmod rina-shim-hv.ko
 sudo rmmod rina-ctrl
+
+# unprepare VMPI-KVM
+pushd .
+cd ../vmpi
+./unprepare-host-kvm.sh
+./unprepare-guest-kvm.sh
+popd
