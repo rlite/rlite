6a72fd35b64f1b72c4ae5bcc0fc7e9203931071f
Vincenzo Maffione <v.maffione@gmail.com>
2016-01-02 11:26:23 +0200

    user: remove unused "application" messages

--- include/rina/rina-application-msg.h
+++ include/rina/rina-application-msg.h
@@ -11,61 +11,17 @@
 /* Message types. They **must** be listed alternating requests with
  * the corresponding responses. */
 enum {
-    RINA_CONF_IPCP_CREATE = 1,
-    RINA_CONF_IPCP_CREATE_RESP,   /* 2 */
-    RINA_CONF_IPCP_DESTROY,       /* 3 */
-    RINA_CONF_IPCP_DESTROY_RESP,  /* 4 */
-    RINA_CONF_ASSIGN_TO_DIF,      /* 5 */
-    RINA_CONF_ASSIGN_TO_DIF_RESP, /* 6 */
-    RINA_CONF_IPCP_CONFIG,        /* 7 */
-    RINA_CONF_IPCP_REGISTER,      /* 8 */
-    RINA_CONF_IPCP_ENROLL,        /* 9 */
-    RINA_CONF_IPCP_DFT_SET,       /* 10 */
-    RINA_CONF_BASE_RESP,          /* 11 */
+    RINA_CONF_IPCP_REGISTER = 1,  /* 1 */
+    RINA_CONF_IPCP_ENROLL,        /* 2 */
+    RINA_CONF_IPCP_DFT_SET,       /* 3 */
+    RINA_CONF_BASE_RESP,          /* 4 */
 
     RINA_CONF_MSG_MAX,
 };
 
-/* Numtables for application <==> ipcm messages exchange. */
+/* Numtables for rina-config <==> ipcm messages exchange. */
 
 extern struct rina_msg_layout rina_conf_numtables[RINA_CONF_MSG_MAX];
-
-/* Application --> IPCM message to create a new IPC process. */
-struct rina_amsg_ipcp_create {
-    rina_msg_t msg_type;
-    uint32_t event_id;
-
-    uint8_t dif_type;
-    struct rina_name ipcp_name;
-} __attribute__((packed));
-
-/* Application --> IPCM message to destroy an IPC process. */
-struct rina_amsg_ipcp_destroy {
-    rina_msg_t msg_type;
-    uint32_t event_id;
-
-    struct rina_name ipcp_name;
-} __attribute__((packed));
-
-/* Application --> IPCM message to assign an IPC process
- * to a DIF. */
-struct rina_amsg_assign_to_dif {
-    rina_msg_t msg_type;
-    uint32_t event_id;
-
-    struct rina_name application_name;
-    struct rina_name dif_name;
-} __attribute__((packed));
-
-/* Application --> IPCM message to configure an IPC process */
-struct rina_amsg_ipcp_config {
-    rina_msg_t msg_type;
-    uint32_t event_id;
-
-    struct rina_name ipcp_name;
-    char *name;
-    char *value;
-} __attribute__((packed));
 
 /* Application --> IPCM message to register an IPC process
  * to another IPC process */
--- user/rina-application-numtables.c
+++ user/rina-application-numtables.c
@@ -3,28 +3,6 @@
 
 
 struct rina_msg_layout rina_conf_numtables[] = {
-    [RINA_CONF_IPCP_CREATE] = {
-        .copylen = sizeof(struct rina_amsg_ipcp_create) -
-                   sizeof(struct rina_name),
-        .names = 1,
-    },
-    [RINA_CONF_IPCP_DESTROY] = {
-        .copylen = sizeof(struct rina_amsg_ipcp_destroy) -
-                   sizeof(struct rina_name),
-        .names = 1,
-    },
-    [RINA_CONF_ASSIGN_TO_DIF] = {
-        .copylen = sizeof(struct rina_amsg_assign_to_dif) -
-                   2 * sizeof(struct rina_name),
-        .names = 2,
-    },
-    [RINA_CONF_IPCP_CONFIG] = {
-        .copylen = sizeof(struct rina_amsg_ipcp_config) -
-                   1 * sizeof(struct rina_name) -
-                   2 * sizeof(char *),
-        .names = 1,
-        .strings = 2,
-    },
     [RINA_CONF_IPCP_REGISTER] = {
         .copylen = sizeof(struct rina_amsg_ipcp_register) -
                    2 * sizeof(struct rina_name),
