ac290670b1981bc5af8504c0f9bcb7067814bfea
Vincenzo Maffione <v.maffione@gmail.com>
2016-01-02 11:26:23 +0200

    tests: command || exit 1

--- tests/1.sh
+++ tests/1.sh
@@ -2,21 +2,21 @@
 
 source env.sh
 
-$RINACONF ipcp-create shim-dummy du.IPCP 1
-$RINACONF ipcp-create shim-dummy du.IPCP 2
-$RINACONF ipcp-create shim-dummy dudu.IPCP 5
+$RINACONF ipcp-create shim-dummy du.IPCP 1 || exit 1
+$RINACONF ipcp-create shim-dummy du.IPCP 2 || exit 1
+$RINACONF ipcp-create shim-dummy dudu.IPCP 5 || exit 1
 
-$RINACONF assign-to-dif dummy.DIF du.IPCP 1
-$RINACONF assign-to-dif dummylo.DIF du.IPCP 2
+$RINACONF assign-to-dif dummy.DIF du.IPCP 1 || exit 1
+$RINACONF assign-to-dif dummylo.DIF du.IPCP 2 || exit 1
 
-$RINACONF application-register dummy.DIF echo-client 1
-$RINACONF application-register dummy.DIF echo-server 1
-$RINACONF application-register dummylo.DIF echo-client 1
+$RINACONF application-register dummy.DIF echo-client 1 || exit 1
+$RINACONF application-register dummy.DIF echo-server 1 || exit 1
+$RINACONF application-register dummylo.DIF echo-client 1 || exit 1
 
-$RINACONF application-unregister dummy.DIF echo-client 1
-$RINACONF application-unregister dummy.DIF echo-server 1
-$RINACONF application-unregister dummylo.DIF echo-client 1
+$RINACONF application-unregister dummy.DIF echo-client 1 || exit 1
+$RINACONF application-unregister dummy.DIF echo-server 1 || exit 1
+$RINACONF application-unregister dummylo.DIF echo-client 1 || exit 1
 
-$RINACONF ipcp-destroy du.IPCP 1
-$RINACONF ipcp-destroy du.IPCP 2
-$RINACONF ipcp-destroy dudu.IPCP 5
+$RINACONF ipcp-destroy du.IPCP 1 || exit 1
+$RINACONF ipcp-destroy du.IPCP 2 || exit 1
+$RINACONF ipcp-destroy dudu.IPCP 5 || exit 1
