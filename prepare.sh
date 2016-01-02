cd68c01aa1785f6a4c529479782e3b2c03f9ecc3
Vincenzo Maffione <v.maffione@gmail.com>
2016-01-02 11:26:23 +0200

    update prepare/unprepare for shim-hv

--- prepare.sh
+++ prepare.sh
@@ -4,6 +4,7 @@ set -x
 
 sudo insmod kernel/rina-ctrl.ko
 sudo insmod kernel/rina-shim-dummy.ko
+sudo insmod kernel/rina-shim-hv.ko
 sudo chmod a+rwx /dev/rina-ipcm-ctrl
 sudo chmod a+rwx /dev/rina-app-ctrl
 sudo chmod a+rwx /dev/rina-io
--- unprepare.sh
+++ unprepare.sh
@@ -3,4 +3,5 @@
 set -x
 
 sudo rmmod rina-shim-dummy.ko
+sudo rmmod rina-shim-hv.ko
 sudo rmmod rina-ctrl
