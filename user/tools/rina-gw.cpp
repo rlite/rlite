6710f9f9d0ee670cbcfc063f3c58f5b478fbbec2
Vincenzo Maffione <v.maffione@gmail.com>
2016-01-02 11:26:23 +0200

    gateway: parsing of rina-gw.conf

--- user/tools/rina-gw.cpp
+++ user/tools/rina-gw.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 #include <map>
+#include <fstream>
+#include <sstream>
+
+#include "rlite/common.h"
 
 using namespace std;
 
 
 int main()
 {
+    const char *confname = "rina-gw.conf";
+    ifstream fin(confname);
+
+    if (fin.fail()) {
+        PE("Failed to open configuration file '%s'\n", confname);
+        return -1;
+    }
+
+    while (!fin.eof()) {
+        string line;
+        string token;
+
+        getline(fin, line);
+
+        istringstream iss(line);
+
+        while (iss >> token) {
+            cout << token << " ";
+        }
+    }
+
     return 0;
 }
