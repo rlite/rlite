535e7ff0b5a1868c34650e5de77f7a30f2599876
Vincenzo Maffione <v.maffione@gmail.com>
2016-01-02 11:26:23 +0200

    rina-applications-numtables.h added

---
+++ include/rina/rina-application-numtables.h
@@ -0,0 +1,11 @@
+#ifndef __RINA_APPLICATION_NUMTABLES_H__
+#define __RINA_APPLICATION_NUMTABLES_H__
+
+#include <rina/rina-utils.h>
+
+
+/* Numtables for application <==> ipcm messages exchange. */
+
+extern struct rina_msg_layout rina_application_numtables[0];
+
+#endif  /* __RINA_APPLICATION_NUMTABLES_H__ */
