f718d901e1193257f240470cbc6dd27ebbb6654c
Vincenzo Maffione <v.maffione@gmail.com>
2016-01-02 11:26:23 +0200

    uipcp: inet4: parse_directory()

--- user/uipcp-shim-inet4.c
+++ user/uipcp-shim-inet4.c
@@ -2,8 +2,82 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <ctype.h>
+#include <arpa/inet.h>
 
 #include "uipcp-container.h"
+
+/*static */int
+parse_directory()
+{
+    const char *dirfile = "/etc/rlite/shim-inet4-dir";
+    FILE *fin = fopen(dirfile, "r");
+    char *linebuf = NULL;
+    size_t sz;
+    size_t n;
+
+    if (!fin) {
+        PE("Could not open directory file '%s'\n", dirfile);
+        return -1;
+    }
+
+    while ((n = getline(&linebuf, &sz, fin)) > 0) {
+        /* I know, strtok_r, strsep, etc. etc. I just wanted to have
+         * some fun ;) */
+        char *nm = linebuf;
+        char *ip, *port, *eol;
+        struct in_addr addr;
+        struct rina_name appl_name;
+        int ret;
+
+        while (*nm != '\0' && isspace(*nm)) nm++;
+        if (*nm == '\0') continue;
+
+        ip = nm;
+        while (*ip != '\0' && !isspace(*ip)) ip++;
+        if (*ip == '\0') continue;
+
+        *ip = '\0';
+        ip++;
+        while (*ip != '\0' && isspace(*ip)) ip++;
+        if (*ip == '\0') continue;
+
+        port = ip;
+        while (*port != '\0' && !isspace(*port)) port++;
+        if (*port == '\0') continue;
+
+        *port = '\0';
+        port++;
+        while (*port != '\0' && isspace(*port)) port++;
+        if (*port == '\0') continue;
+
+        eol = port;
+        while (*eol != '\0' && !isspace(*eol)) eol++;
+        if (*eol != '\0') *eol = '\0';
+
+        ret = inet_pton(AF_INET, ip, &addr);
+        if (ret != 1) {
+            PE("Invalid IP address '%s'\n", ip);
+            continue;
+        }
+
+        ret = rina_name_from_string(nm, &appl_name);
+        if (ret) {
+            PE("Invalid name '%s'\n", nm);
+            continue;
+        }
+
+        printf("oho '%s' '%s'[%d] '%d'\n", nm, ip, ret, atoi(port));
+    }
+
+    if (linebuf) {
+        free(linebuf);
+    }
+
+    fclose(fin);
+
+    return 0;
+}
 
 static int
 shim_inet4_appl_register(struct rlite_evloop *loop,
