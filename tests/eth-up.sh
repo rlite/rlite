05d2c2a01e2a1250e1de230438ad5c19581fa936
Vincenzo Maffione <v.maffione@gmail.com>
2016-01-02 11:26:23 +0200

    tests: use eth0 as a test NIC device

--- tests/eth-up.sh
+++ tests/eth-up.sh
@@ -4,7 +4,7 @@ source tests/prologue.sh
 source tests/env.sh
 
 $RINACONF ipcp-create shim-eth e.IPCP 1
-$RINACONF ipcp-config e.IPCP 1 netdev ens4
+$RINACONF ipcp-config e.IPCP 1 netdev eth0
 $RINACONF ipcp-config e.IPCP 1 dif e.DIF
 
 source tests/epilogue.sh
