d8c2a5cf5e9c76179196db4a1f5228d9bc5136f5
Vincenzo Maffione <v.maffione@gmail.com>
2016-01-02 11:26:23 +0200

    gateway: complete parsing

--- user/tools/rina-gw.cpp
+++ user/tools/rina-gw.cpp
@@ -2,11 +2,81 @@
 #include <map>
 #include <fstream>
 #include <sstream>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 #include "rlite/common.h"
+#include "rlite/utils.h"
 
 using namespace std;
 
+
+struct InetName {
+    struct sockaddr_in addr;
+
+    InetName() { memset(&addr, 0, sizeof(addr)); }
+    InetName(const struct sockaddr_in& a) : addr(a) { }
+
+    bool operator<(const InetName& other) const;
+};
+
+bool
+InetName::operator<(const InetName& other) const
+{
+    if (addr.sin_addr.s_addr != other.addr.sin_addr.s_addr) {
+        return addr.sin_addr.s_addr < other.addr.sin_addr.s_addr;
+    }
+
+    return addr.sin_port < other.addr.sin_port;
+}
+
+struct RinaName {
+    string name_s;
+    struct rina_name name_r;
+
+    RinaName() { memset(&name_r, 0, sizeof(name_r)); }
+    RinaName(const string& n);
+    RinaName(const RinaName& other);
+    ~RinaName() { rina_name_free(&name_r); }
+
+    bool operator<(const RinaName& other) const {
+        return name_s < other.name_s;
+    }
+};
+
+RinaName::RinaName(const string& n) : name_s(n)
+{
+    if (rina_name_from_string(n.c_str(), &name_r)) {
+        throw std::bad_alloc();
+    }
+}
+
+RinaName::RinaName(const RinaName& other)
+{
+    name_s = other.name_s;
+    if (rina_name_copy(&name_r, &other.name_r)) {
+        throw std::bad_alloc();
+    }
+}
+
+struct Gateway {
+    /* Used to map IP:PORT --> RINA_NAME, when
+     * receiving TCP connection requests from the INET world
+     * towards the RINA world. */
+    map<InetName, RinaName> srv_map;
+
+    /* Used to map RINA_NAME --> IP:PORT, when
+     * receiving flow allocation requests from the RINA world
+     * towards the INET world. */
+    map<RinaName, InetName> dst_map;
+};
+
+Gateway gw;
 
 int main()
 {
@@ -18,16 +88,64 @@ int main()
         return -1;
     }
 
-    while (!fin.eof()) {
-        string line;
+    for (unsigned int lines_cnt = 1; !fin.eof(); lines_cnt++) {
+        vector<string> tokens;
         string token;
+        string line;
+        int ret;
 
         getline(fin, line);
 
-        istringstream iss(line);
+        {
+            istringstream iss(line);
+            struct sockaddr_in inet_addr;
 
-        while (iss >> token) {
-            cout << token << " ";
+            while (iss >> token) {
+                tokens.push_back(token);
+            }
+
+            if (tokens.size() < 4) {
+                if (tokens.size()) {
+                    PI("Invalid configuration entry at line %d\n", lines_cnt);
+                }
+                continue;
+            }
+
+            try {
+                memset(&inet_addr, 0, sizeof(inet_addr));
+                inet_addr.sin_family = AF_INET;
+                inet_addr.sin_port = atoi(tokens[3].c_str());
+                if (inet_addr.sin_port >= 65536) {
+                    PI("Invalid configuration entry at line %d: "
+                       "invalid port number '%s'\n", lines_cnt,
+                       tokens[3].c_str());
+                }
+                ret = inet_pton(AF_INET, tokens[2].c_str(),
+                                &inet_addr.sin_addr);
+                if (ret != 1) {
+                    PI("Invalid configuration entry at line %d: "
+                       "invalid IP address '%s'\n", lines_cnt,
+                       tokens[2].c_str());
+                    continue;
+                }
+
+                InetName inet_name(inet_addr);
+                RinaName rina_name(tokens[1]);
+
+                if (tokens[0] == "SRV") {
+                    gw.srv_map.insert(make_pair(inet_name, rina_name));
+
+                } else if (tokens[0] == "DST") {
+                    gw.dst_map.insert(make_pair(rina_name, inet_name));
+
+                } else {
+                    PI("Invalid configuration entry at line %d: %s is "
+                       "unknown\n", lines_cnt, tokens[0].c_str());
+                    continue;
+                }
+            } catch (std::bad_alloc) {
+                PE("Out of memory while processing configuration file\n");
+            }
         }
     }
 
