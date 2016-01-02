fb1114704a736ff1d27c6031d848ecc4103c1bc8
Vincenzo Maffione <v.maffione@gmail.com>
2016-01-02 11:26:23 +0200

    kernel: inet4: rx path sketched with workqueues

--- kernel/shim-inet4.c
+++ kernel/shim-inet4.c
@@ -35,10 +35,17 @@
 #include <linux/net.h>
 #include <linux/file.h>
 #include <linux/version.h>
+#include <net/sock.h>
 
 
 struct rina_shim_inet4 {
     struct ipcp_entry *ipcp;
+};
+
+struct shim_inet4_flow {
+    struct socket *sock;
+    struct work_struct rxw;
+    void (*sk_data_ready)(struct sock *sk);
 };
 
 static void *
@@ -68,22 +75,93 @@ rina_shim_inet4_destroy(struct ipcp_entry *ipcp)
     PD("IPCP [%p] destroyed\n", priv);
 }
 
+static void
+inet4_rx_worker(struct work_struct *w)
+{
+    struct shim_inet4_flow *priv =
+            container_of(w, struct shim_inet4_flow, rxw);
+    struct msghdr msghdr;
+    struct socket *sock = priv->sock;
+    struct rina_buf *rb;
+    struct iovec iov;
+    int size;
+
+    PD("called\n");
+
+    memset(&msghdr, 0, sizeof(msghdr));
+    msghdr.msg_flags = MSG_DONTWAIT;
+
+    /* Peek length from socket rxq. TODO */
+    rb = rina_buf_alloc(1000, 3, GFP_ATOMIC);
+    if (!rb) {
+        PE("Out of memory\n");
+        return;
+    }
+
+    iov.iov_base = RINA_BUF_DATA(rb);
+    iov.iov_len = rb->len;
+    msghdr.msg_iov = &iov;
+    msghdr.msg_iovlen = 1;
+
+printk(KERN_CRIT "GOT HERE\n");
+
+    size = sock->ops->recvmsg(NULL, sock, &msghdr, rb->len,
+                              MSG_DONTWAIT | MSG_TRUNC);
+/*    size = kernel_recvmsg(sk->sk_socket, &msghdr, (struct kvec *)&iov, 1,
+                          rb->len, msghdr.msg_flags); */
+    if (unlikely(size < 0)) {
+        if (size == -EAGAIN) {
+            PD("recvmsg(): got EAGAIN\n");
+        } else {
+            PE("recvmsg(): %d\n", size);
+        }
+    }
+
+    PD("read %d bytes\n", size);
+
+    rina_buf_free(rb);
+}
+
+static void
+inet4_data_ready(struct sock *sk)
+{
+    struct shim_inet4_flow *priv = sk->sk_user_data;
+
+    schedule_work(&priv->rxw);
+}
+
 static int
 rina_shim_inet4_flow_init(struct ipcp_entry *ipcp, struct flow_entry *flow)
 {
+    struct shim_inet4_flow *priv;
     struct socket *sock;
     int err;
+
+    priv = kmalloc(sizeof(*priv), GFP_ATOMIC);
+    if (!priv) {
+        PE("Out of memory\n");
+        return -1;
+    }
 
     /* This increments the file descriptor reference counter. */
     sock = sockfd_lookup(flow->cfg.fd, &err);
     if (!sock) {
         PE("Cannot find socket corresponding to file descriptor %d\n", flow->cfg.fd);
+        kfree(priv);
         return err;
     }
 
+    write_lock_bh(&sock->sk->sk_callback_lock);
+    priv->sk_data_ready = sock->sk->sk_data_ready;
+    sock->sk->sk_data_ready = inet4_data_ready;
+    sock->sk->sk_user_data = priv;
+    write_unlock_bh(&sock->sk->sk_callback_lock);
+
     PD("Got socket %p\n", sock);
 
-    flow->priv = sock;
+    priv->sock = sock;
+    INIT_WORK(&priv->rxw, inet4_rx_worker);
+    flow->priv = priv;
 
     return 0;
 }
@@ -92,16 +170,25 @@ static int
 rina_shim_inet4_flow_deallocated(struct ipcp_entry *ipcp,
                                  struct flow_entry *flow)
 {
-    struct socket *sock = flow->priv;
+    struct shim_inet4_flow *priv = flow->priv;
+    struct socket *sock;
 
-    if (!sock) {
+    if (!priv) {
         return 0;
     }
+
+    sock = priv->sock;
+
+    write_lock_bh(&sock->sk->sk_callback_lock);
+    sock->sk->sk_data_ready = priv->sk_data_ready;
+    sock->sk->sk_user_data = NULL;
+    write_unlock_bh(&sock->sk->sk_callback_lock);
 
     /* Decrement the file descriptor reference counter, in order to
      * match flow_init(). */
     fput(sock->file);
     flow->priv = NULL;
+    kfree(priv);
 
     PD("Released socket %p\n", sock);
 
@@ -113,7 +200,7 @@ rina_shim_inet4_sdu_write(struct ipcp_entry *ipcp,
                       struct flow_entry *flow,
                       struct rina_buf *rb, bool maysleep)
 {
-    struct socket *sock = flow->priv;
+    struct shim_inet4_flow *priv= flow->priv;
     struct msghdr msghdr;
     struct iovec iov;
     int ret;
@@ -125,11 +212,11 @@ rina_shim_inet4_sdu_write(struct ipcp_entry *ipcp,
     msghdr.msg_flags = MSG_DONTWAIT;
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4,0,0)
     iov_iter_init(&msghdr.msg_iter, WRITE, &iov, 1, rb->len);
-    ret = sock_sendmsg(sock, &msghdr, rb->len);
+    ret = sock_sendmsg(priv->sock, &msghdr, rb->len);
 #else
     msghdr.msg_iov = &iov;
     msghdr.msg_iovlen = 1;
-    ret = kernel_sendmsg(sock, &msghdr, (struct kvec *)&iov, 1, rb->len);
+    ret = kernel_sendmsg(priv->sock, &msghdr, (struct kvec *)&iov, 1, rb->len);
 #endif
 
     if (unlikely(ret < 0)) {
