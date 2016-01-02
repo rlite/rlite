f4021ae66ecea43d521ab91c3f7e2331977d3da9
Vincenzo Maffione <v.maffione@gmail.com>
2016-01-02 11:26:23 +0200

    tests: add normal over shim hv tests

---
+++ tests/normal-hv-down.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source tests/prologue.sh
+source tests/env.sh
+
+if [ "$1" != "g" -a "$1" != "h" ]; then
+    echo "First argument required ('h' or 'g')"
+    exit 1
+fi
+
+if [ "$1" == "h" ]; then
+    SHIPCP="h.IPCP"
+    NORMIPCP="nh.IPCP"
+else
+    SHIPCP="g.IPCP"
+    NORMIPCP="ng.IPCP"
+fi
+
+# it's not necessary to unregister, we can rely on
+# auto-unregistration
+$RINACONF ipcp-destroy $NORMIPCP 1
+$RINACONF ipcp-destroy $SHIPCP 1
+
+source tests/epilogue.sh
---
+++ tests/normal-hv-up.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+source tests/prologue.sh
+source tests/env.sh
+
+if [ "$1" != "g" -a "$1" != "h" ]; then
+    echo "First argument required ('h' or 'g')"
+    exit
+fi
+
+# Try to extract the last vmpi-id from the kernel log
+vmpi_id=$(dmesg | grep -o "Provider [01]:[0-9]\+" | tail -n1 | grep -o "[0-9]\+$")
+if [ -z ${vmpi_id} ]; then
+    echo "No registered provider"
+fi
+
+if [ "$1" == "h" ]; then
+    SHIPCP="h.IPCP"
+    NORMIPCP="nh.IPCP"
+    ADDR=21
+else
+    SHIPCP="g.IPCP"
+    NORMIPCP="ng.IPCP"
+    ADDR=22
+fi
+
+$RINACONF ipcp-create shim-hv $SHIPCP 1
+$RINACONF assign-to-dif d.DIF $SHIPCP 1
+$RINACONF ipcp-config $SHIPCP 1 vmpi-id ${vmpi_id}
+$RINACONF ipcp-create normal $NORMIPCP 1
+$RINACONF assign-to-dif n.DIF $NORMIPCP 1
+$RINACONF ipcp-config $NORMIPCP 1 address $ADDR
+$RINACONF ipcp-register d.DIF $NORMIPCP 1
+if [ "$1" == "h" ]; then
+    # Host enroll to the guest --> guest up script must be run first
+    $RINACONF ipcp-enroll n.DIF $NORMIPCP 1 ng.IPCP 1 d.DIF
+else
+    # The guest normal DIF knows that rinaperf server is registered
+    # on the host, so the host must run the server and the guest
+    # must run the client
+    $RINACONF ipcp-dft-set $NORMIPCP 1 rinaperf-data server 21
+fi
+
+source tests/epilogue.sh
