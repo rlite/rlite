dda9b20e8d3e2a9547f361350b1a1b906e32beeb
Vincenzo Maffione <v.maffione@gmail.com>
2016-08-05 10:02:01 +0200

    move scripts to their directory

--- tests/stress.sh
+++ tests/stress.sh
@@ -2,7 +2,7 @@
 
 for i in $(seq 1 5 4092); do
     for j in $(seq 1 5); do
-        thr=$(user/rinaperf -t perf -c 100000 -s $i | grep "Mbps" | awk '{printf "%s\n", $4}')
+        thr=$(rinaperf -t perf -c 100000 -s $i | grep "Mbps" | awk '{printf "%s\n", $4}')
         echo "$i $thr"
     done
 done
