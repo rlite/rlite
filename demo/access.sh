f6ce747665fb81e255c723b542b9ed033d0bc0bd
Vincenzo Maffione <v.maffione@gmail.com>
2016-12-23 15:06:31 +0100

    demo: access.sh: remove bash-isms

--- demo/access.sh
+++ demo/access.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 MACHINE_ID=$1
-if [ "${MACHINE_ID}" == "" ]; then
+if [ "$MACHINE_ID" == "" ]; then
 	echo "usage: $0 NODE_NAME"
 	exit 255
 fi
 
 SSH_PORT=$(grep "\<${MACHINE_ID}\>" demo.map | awk '{print $2}')
-if [ "${SSH_PORT}" == "" ]; then
+if [ "$SSH_PORT" == "" ]; then
 	echo "Error: Node ${MACHINE_ID} unknown"
 	exit 255
 fi
