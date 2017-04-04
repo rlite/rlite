9a65b609332a71ee07d4ebdc31e027b84a77e603
Vincenzo Maffione <v.maffione@gmail.com>
2017-04-04 17:05:46 +0200

    tests: parallel clients/servers: add command line options

--- tests/parallel-clients.sh
+++ tests/parallel-clients.sh
@@ -1,6 +1,51 @@
 #!/bin/bash
 
+function usage {
+    echo "$0 [ -n NUM_CLIENTS ] [ -p PER_CLIENT_FLOWS ]"
+}
+
+N=100
+P=1
+
+# Option parsing
+while [[ $# > 0 ]]
+do
+    key="$1"
+    case $key in
+        "-n")
+        if [ -n "$2" ]; then
+            N="$2"
+            shift
+        else
+            echo "-n requires a numeric argument"
+            exit 255
+        fi
+        ;;
+
+        "-p")
+        if [ -n "$2" ]; then
+            P="$2"
+            shift
+        else
+            echo "-p requires a numeric argument"
+            exit 255
+        fi
+        ;;
+
+        "-h")
+            usage
+            exit 0
+        ;;
+
+        *)
+        echo "Unknown option '$key'"
+        exit 255
+        ;;
+    esac
+    shift
+done
+
 # Run multiple client instances
-for i in $(seq 1 100); do
-    rinaperf -z rinaperf-server:$i -t perf -c 1000 -i 10000 &
+for i in $(seq 1 $N); do
+    rinaperf -z rinaperf-server:$i -p $P -t perf -c 1000 -i 10000 &
 done
--- tests/parallel-servers.sh
+++ tests/parallel-servers.sh
@@ -1,6 +1,41 @@
 #!/bin/bash
 
+function usage {
+    echo "$0 [ -n NUM_SERVERS ]"
+}
+
+N=100
+
+# Option parsing
+while [[ $# > 0 ]]
+do
+    key="$1"
+    case $key in
+        "-n")
+        if [ -n "$2" ]; then
+            N="$2"
+            shift
+        else
+            echo "-n requires a numeric argument"
+            exit 255
+        fi
+        ;;
+
+        "-h")
+            usage
+            exit 0
+        ;;
+
+        *)
+        echo "Unknown option '$key'"
+        exit 255
+        ;;
+    esac
+    shift
+done
+
 # Run multiple server instances
-for i in $(seq 1 100); do
+pkill rinaperf
+for i in $(seq 1 $N); do
     rinaperf -l -z rinaperf-server:$i &
 done
