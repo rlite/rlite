60aa99edae714b4c0f4df055ae7b229bea7eea0a
Vincenzo Maffione <v.maffione@gmail.com>
2017-04-22 10:12:02 +0200

    user, kernel: enable A timer by default

--- include/rlite/common.h
+++ include/rlite/common.h
@@ -218,7 +218,7 @@ struct rl_flow_config {
 
 #define RL_MPL_MSECS_DFLT       1000
 #define RL_RTX_MSECS_DFLT       1000
-#define RL_A_MSECS_DFLT         0
+#define RL_A_MSECS_DFLT         20
 #define RL_DATA_RXMS_MAX_DFLT   10
 
 /* We don't currently have a good way to understand what flow specs
