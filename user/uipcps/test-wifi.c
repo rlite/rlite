6baa25799459448e810f609d3872a743fb0e5c2b
Michal Koutenský <koutak.m@gmail.com>
2017-10-03 12:00:39 +0200

    scan as a separate function; block while waiting for events

--- user/uipcps/test-wifi.c
+++ user/uipcps/test-wifi.c
@@ -23,6 +23,7 @@
 
 #define _GNU_SOURCE
 
+#include <poll.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -32,11 +33,25 @@
 
 #include "../uipcps/wpa-supplicant/wpa_ctrl.h"
 
-static struct wpa_ctrl *ctrl_conn = NULL;
+char *create_ctrl_path(char *ctrl_dir, char *inf)
+{
+    /* parameter to wpa_ctrl_open needs to be path in config + interface */
+    size_t len_dir = strlen(ctrl_dir);
+    size_t len_inf = strlen(inf);
+    char *ctrl_path = malloc(len_dir + len_inf + 2);
+    if (!ctrl_path) {
+        return NULL;
+    }
+    strncpy(ctrl_path, ctrl_dir, len_dir);
+    ctrl_path[len_dir] = '/';
+    strncpy(ctrl_path + len_dir + 1, inf, len_inf);
+    return ctrl_path;
+}
 
 /* sends a command to the control interface */
 /* no checking done */
-void send_cmd(const char *cmd) {
+void send_cmd(struct wpa_ctrl *ctrl_conn, const char *cmd)
+{
     char buf[4096];
     size_t len = sizeof(buf) - 1;
 
@@ -51,18 +66,45 @@ void send_cmd(const char *cmd) {
 
 /* reads a message from the control interface */
 /* no checking done */
-bool recv_msg() {
-    char buf[4096];
-    size_t len = sizeof(buf) - 1;
-
+size_t recv_msg(struct wpa_ctrl *ctrl_conn, char *buf, size_t len)
+{
+    len--;
     wpa_ctrl_recv(ctrl_conn, buf, &len);
     buf[len] = '\0';
 
     printf("%s", buf);
-    if (len > 0 && buf[len - 1] != '\n')
+    if (len > 0 && buf[len - 1] != '\n') {
         printf("\n");
+    }
+    return len;
+}
 
-    return !strncmp(buf, "<3>CTRL-EVENT-SCAN-RESULTS", 26);
+void scan(struct wpa_ctrl *ctrl_conn)
+{
+    static const int len = 4096;
+    char buf[len];
+    int ret;
+
+    struct pollfd ctrl_pollfd;
+    ctrl_pollfd.fd = wpa_ctrl_get_fd(ctrl_conn);
+    ctrl_pollfd.events = POLLIN;
+
+    send_cmd(ctrl_conn, "SCAN");
+
+    /* loop until we get a 'scanning done' message */
+    do {
+        ctrl_pollfd.events = POLLIN;
+        ret = poll(&ctrl_pollfd, 1, 5000);
+        if (ret == -1) {
+            perror("poll()");
+            return;
+        }
+        if (ctrl_pollfd.revents & POLLIN) {
+            recv_msg(ctrl_conn, buf, len);
+        }
+    } while (strncmp(buf, "<3>CTRL-EVENT-SCAN-RESULTS", 26));
+
+    send_cmd(ctrl_conn, "SCAN_RESULTS");
 }
 
 int main(int argc, char **argv)
@@ -116,23 +158,19 @@ int main(int argc, char **argv)
         }
     }
 
+    struct wpa_ctrl *ctrl_conn = NULL;
+
     /* delay to make sure it's started */
     sleep(2);
 
-    /* parameter to wpa_ctrl_open needs to be path in config + interface */
-    /* ugly */
-    char *ctrl_path = malloc(strlen(ctrl_dir) + strlen(inf) + 2);
+    char *ctrl_path = create_ctrl_path(ctrl_dir, inf);
     if (!ctrl_path) {
-        fprintf(stderr, "Failed to allocate memory, terminating.\n");
         if (start_supplicant) {
             kill(pid, SIGTERM);
             waitpid(pid, &status, 0);
         }
         return 1;
     }
-    strncpy(ctrl_path, ctrl_dir, strlen(ctrl_dir));
-    ctrl_path[strlen(ctrl_dir)] = '/';
-    strncpy(ctrl_path + strlen(ctrl_dir) + 1, inf, strlen(inf));
 
     /* get the control handle */
     ctrl_conn = wpa_ctrl_open(ctrl_path);
@@ -145,21 +183,12 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    free(ctrl_path);
+
     /* attach so we can send control messages */
     wpa_ctrl_attach(ctrl_conn);
 
-    send_cmd("SCAN");
-
-    /* loop until we get a 'scanning done' message */
-    bool done_scanning = false;
-    while (!done_scanning) {
-        while (wpa_ctrl_pending(ctrl_conn) > 0) {
-            /* check is done in recv_msg */
-            done_scanning = recv_msg();
-        };
-    }
-
-    send_cmd("SCAN_RESULTS");
+    scan(ctrl_conn);
 
     /* cleanup */
     wpa_ctrl_detach(ctrl_conn);
@@ -168,4 +197,5 @@ int main(int argc, char **argv)
         kill(pid, SIGTERM);
         waitpid(pid, &status, 0);
     }
+    return 0;
 }
