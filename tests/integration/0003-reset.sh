e22d3e44fcbea87ffff2cd389fe51aac16678590
Vincenzo Maffione <v.maffione@gmail.com>
2017-11-04 18:20:31 +0100

    tests: add test for rlite-ctl reset

---
+++ tests/integration/0003-reset.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create some IPCPs
+rlite-ctl ipcp-create x normal xx || exit 1
+rlite-ctl ipcp-create y shim-loopback yy || exit 1
+rlite-ctl ipcp-create z normal zz || exit 1
+# Check that they are in kernel
+rlite-ctl | grep dif_name | grep "\<x\>" | grep "\<xx\>" || exit 1
+rlite-ctl | grep dif_name | grep "\<y\>" | grep "\<yy\>" || exit 1
+rlite-ctl | grep dif_name | grep "\<z\>" | grep "\<zz\>" || exit 1
+# Reset
+rlite-ctl reset
+# Check that the IPCPs are gone
+rlite-ctl | grep dif_name && exit 1
+exit 0
