e5d060569f66518ea7f49e95d35526cf7c8e8fa6
Vincenzo Maffione <v.maffione@gmail.com>
2017-11-04 18:04:08 +0100

    tests: prepare and cleanup test environment for each test

---
+++ tests/integration/0002-normal-ipcp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Create a normal IPCP
+rlite-ctl ipcp-create x normal xx || exit 1
+# Check that kernel knows it's there
+rlite-ctl | grep dif_name | grep "\<x\>" | grep "\<xx\>"
+# Check that it responds to RIB queries
+rlite-ctl dif-rib-show xx
--- tests/run-integration-tests.sh
+++ tests/run-integration-tests.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
+function abort_prepare() {
+    echo ">>> FAILED TO PREPARE TEST ENVIRONMENT"
+    exit 1
+}
+
+function abort_cleanup() {
+    echo ">>> FAILED TO CLEANUP TEST ENVIRONMENT"
+    exit 1
+}
+
 for t in $(ls tests/integration/*); do
     echo ">>> Running integration test \"${t}\""
-    sudo rlite-uipcps -d
+    # Prepare test environment
+    sudo modprobe rlite || abort_prepare
+    sudo modprobe rlite-normal || abort_prepare
+    sudo modprobe rlite-shim-loopback || abort_prepare
+    sudo rlite-uipcps -d || abort_prepare
     sudo ${t}
     retcode="$?"
-    sudo kill $(cat /run/rlite/uipcps.pid)
+    sudo kill $(cat /run/rlite/uipcps.pid) || abort_cleanup
+    sudo rmmod rlite-normal || abort_cleanup
+    sudo rmmod rlite-shim-loopback || abort_cleanup
+    sudo rmmod rlite || abort_cleanup
     if [ "$retcode" != "0" ]; then
         echo ">>> FAILED"
         exit 1
