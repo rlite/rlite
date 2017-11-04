49da89d940d08258ec790f006cde49a6ee8d8a8c
Vincenzo Maffione <v.maffione@gmail.com>
2017-11-04 18:20:31 +0100

    tests: add test for rlite-ctl reset

--- tests/integration/0002-normal-ipcp.sh
+++ tests/integration/0002-normal-ipcp.sh
@@ -6,3 +6,4 @@ rlite-ctl ipcp-create x normal xx || exit 1
 rlite-ctl | grep dif_name | grep "\<x\>" | grep "\<xx\>"
 # Check that it responds to RIB queries
 rlite-ctl dif-rib-show xx
+rlite-ctl dif-routing-show xx
