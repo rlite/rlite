1cf6080a0dfd833fd977457ce9ef09a1e6755978
Vincenzo Maffione <v.maffione@gmail.com>
2017-11-05 19:47:38 +0100

    tests: add integration test for deferred registration

---
+++ tests/integration/0006-register-delayed.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create two normal IPCPs
+rlite-ctl ipcp-create x normal xx || exit 1
+#rlite-ctl ipcp-create y shim-loopback yy || exit 1  # TODO
+rlite-ctl ipcp-create y normal yy || exit 1
+# Register the one into the other
+rlite-ctl ipcp-register x yy || exit 1
+# Check that registration gets deferred
+rlite-ctl regs-show | grep "\<name\>" | grep "\<x\>" && exit 1
+# Enable the first one as an enroller
+rlite-ctl ipcp-enroller-enable x || exit 1
+# Check that the pending registration happened now
+rlite-ctl regs-show | grep "\<name\>" | grep "\<x\>" || exit 1
