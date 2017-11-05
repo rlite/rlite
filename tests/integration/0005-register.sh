fc6ae1f7ff93e0cfce98fbac7f17c73a6ed96bf2
Vincenzo Maffione <v.maffione@gmail.com>
2017-11-05 19:40:59 +0100

    tests: add integration test for registration after enrollment enable

---
+++ tests/integration/0005-register.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Create two normal IPCPs
+rlite-ctl ipcp-create x normal xx || exit 1
+#rlite-ctl ipcp-create y shim-loopback yy || exit 1  # TODO
+rlite-ctl ipcp-create y normal yy || exit 1
+# Enable a normal IPCP as enroller and register it into the other
+rlite-ctl ipcp-enroller-enable x || exit 1
+rlite-ctl ipcp-register x yy || exit 1
+# Check that the registration is known by the kernel
+rlite-ctl regs-show | grep "\<name\>" | grep "\<x\>" || exit 1
