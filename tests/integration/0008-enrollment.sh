fecb27f5ed1f14eaf0c3ac25bdef37ba5c122962
Vincenzo Maffione <v.maffione@gmail.com>
2017-11-06 17:33:04 +0100

    tests: add integration test for enrollment

---
+++ tests/integration/0008-enrollment.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create two normal IPCPs and enroll one to the other,
+# using a third IPCP as a N-1 DIF.
+rlite-ctl ipcp-create x normal dd || exit 1
+rlite-ctl ipcp-create y normal dd || exit 1
+rlite-ctl ipcp-create z normal zz || exit 1
+rlite-ctl ipcp-enroller-enable x || exit 1
+rlite-ctl ipcp-enroller-enable z || exit 1
+rlite-ctl ipcp-register x zz || exit 1
+rlite-ctl ipcp-register y zz || exit 1
+rlite-ctl ipcp-enroll y dd zz x
+# Reset to wait for all the flows to go away
+rlite-ctl reset
