06186df32fabdd46e803cca3d3982102f8e1ffe9
Vincenzo Maffione <v.maffione@gmail.com>
2017-11-06 21:48:10 +0100

    tests: add integration test for dif-policy-mod

---
+++ tests/integration/0010-policy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Create a normal IPCP and test the dif-policy-mod command
+rlite-ctl ipcp-create x normal dd || exit 1
+rlite-ctl ipcp-enroller-enable x || exit 1
+
+# Expect success on the following ones
+rlite-ctl dif-policy-mod dd address-allocator manual || exit 1
+rlite-ctl dif-policy-mod dd address-allocator distributed || exit 1
+rlite-ctl dif-policy-mod dd routing link-state || exit 1
+rlite-ctl dif-policy-mod dd routing link-state-lfa || exit 1
+
+# Expect failure on the following ones
+rlite-ctl dif-policy-mod dd wrong-component manual && exit 1
+rlite-ctl dif-policy-mod dd address-allocator wrong-policy && exit 1
+rlite-ctl dif-policy-mod dd flow-allocator wrong-policy && exit 1
+exit 0
