2bb6ae1e1b140172078421caf17753063a739f79
Vincenzo Maffione <v.maffione@gmail.com>
2017-11-09 18:56:38 +0100

    configure: simplify generation of kernel Makefile

--- unprepare.sh
+++ unprepare.sh
@@ -9,19 +9,6 @@ fi
 
 sudo rm -rf /run/rlite
 
-if [ WITH_VMPI == "y" ]; then
-    sudo rmmod rlite-shim-hv.ko
-fi
-
-if [ WITH_VMPI == "y" ]; then
-    # unprepare VMPI-KVM
-    pushd .
-    cd kernel/vmpi
-    ./unprepare-host-kvm.sh
-    ./unprepare-guest-kvm.sh
-    popd
-fi
-
 sudo rmmod rlite-shim-udp4.ko
 sudo rmmod rlite-shim-tcp4.ko
 sudo rmmod rlite-normal.ko
