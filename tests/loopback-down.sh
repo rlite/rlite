1ccf54fba80c88f0bb49a3c8f9ffc7acc0bd2787
Vincenzo Maffione <v.maffione@gmail.com>
2017-11-09 11:35:12 +0100

    tests: remove more obsolete test scripts

--- tests/loopback-down.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source tests/prologue.sh
-source tests/env.sh
-
-sudo rlite-ctl ipcp-destroy d.IPCP:1
-
-source tests/epilogue.sh
--- tests/loopback-up.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source tests/prologue.sh
-source tests/env.sh
-
-sudo rlite-ctl ipcp-create d.IPCP:1 shim-loopback d.DIF
-sudo rlite-ctl ipcp-config d.IPCP:1 queued 1
-
-source tests/epilogue.sh
