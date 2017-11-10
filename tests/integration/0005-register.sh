9c3d3a5e364c6e28d3598378c3f8bd2d3e11b1aa
Vincenzo Maffione <v.maffione@gmail.com>
2017-11-10 09:07:30 +0100

    tests: cleanup TODO lines

--- tests/integration/0005-register.sh
+++ tests/integration/0005-register.sh
@@ -2,7 +2,6 @@
 
 # Create two normal IPCPs
 rlite-ctl ipcp-create x normal xx || exit 1
-#rlite-ctl ipcp-create y shim-loopback yy || exit 1  # TODO
 rlite-ctl ipcp-create y normal yy || exit 1
 # Enable a normal IPCP as enroller and register it into the other
 rlite-ctl ipcp-enroller-enable x || exit 1
