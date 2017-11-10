912f4c955ae467ef3d8ec6d9fa429a223536c987
Vincenzo Maffione <v.maffione@gmail.com>
2017-11-10 08:59:31 +0100

    tests: add integration test for shim-loopback

---
+++ tests/integration/0014-shim-loopback.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function cleanup() {
+	pkill rinaperf
+	return 0
+}
+
+function abort() {
+	cleanup
+	exit 1
+}
+
+rlite-ctl ipcp-create sl shim-loopback dd || abort
+rlite-ctl ipcp-config sl queued 1 || abort
+rinaperf -lw -z rpinstance7 || abort
+rinaperf -z rpinstance7 -p2 -c 4 -i 0 || abort
+rinaperf -lw -z rpinstance8 || abort
+rinaperf -z rpinstance8  -c 2 -i 0 || abort
+rinaperf -z rpinstance7  -c 2 -i 0 || abort
+rlite-ctl ipcp-destroy sl || abort
+cleanup
