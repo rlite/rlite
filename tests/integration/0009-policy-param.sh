3c3655d9d9813bfb4308db4d80c84b509bd7e158
Michal Koutenský <koutak.m@gmail.com>
2017-11-20 18:11:30 +0100

    tests: add range tests for policy_param_mod, fix failing policy_param_list tests

--- tests/integration/0009-policy-param.sh
+++ tests/integration/0009-policy-param.sh
@@ -15,6 +15,8 @@ rlite-ctl dif-policy-param-mod dd resource-allocator reliable-n-flows true || ex
 rlite-ctl dif-policy-param-mod dd resource-allocator broadcast-enroller true || exit 1
 rlite-ctl dif-policy-param-mod dd rib-daemon refresh-intval 10 || exit 1
 rlite-ctl dif-policy-param-mod dd routing age-incr-intval 10 || exit 1
+rlite-ctl dif-policy-param-mod dd address-allocator nack-wait-secs 0 || exit 1
+rlite-ctl dif-policy-param-mod dd address-allocator nack-wait-secs 99 || exit 1
 
 # Expect failure on the following ones
 rlite-ctl dif-policy-param-mod dd wrong-component timeout 300 && exit 1
@@ -22,4 +24,6 @@ rlite-ctl dif-policy-param-mod dd enrollment wrong-parameter 300 && exit 1
 rlite-ctl dif-policy-param-mod dd enrollment timeout wrong-value && exit 1
 rlite-ctl dif-policy-param-mod dd resource-allocator reliable-flows 1023 && exit 1
 rlite-ctl dif-policy-param-mod dd rib-daemon refresh-intval false && exit 1
+rlite-ctl dif-policy-param-mod dd address-allocator nack-wait-secs -1 && exit 1
+rlite-ctl dif-policy-param-mod dd address-allocator nack-wait-secs 100 && exit 1
 exit 0
