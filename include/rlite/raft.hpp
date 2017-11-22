d473ff3a904e1b919a7bd01ef9383b2d3fefb026
Vincenzo Maffione <v.maffione@gmail.com>
2017-11-22 12:20:35 +0100

    libs: add placeholders for a raft library

---
+++ include/rlite/raft.hpp
@@ -0,0 +1,33 @@
+/*
+ * RAFT protocol for fault-tolerant DIF internal components.
+ *
+ * Copyright (C) 2017 Nextworks
+ * Author: Vincenzo Maffione <v.maffione@gmail.com>
+ *
+ * This file is part of rlite.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#ifndef __RAFT_H__
+#define __RAFT_H__
+
+/* Raft state machine. */
+class RaftSM {
+public:
+    RaftSM();
+};
+
+#endif /* __RAFT_H__ */
