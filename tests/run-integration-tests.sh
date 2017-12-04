250bbe7e2df56738fc5c6cef1dc8b80e3c1103a0
Vincenzo Maffione <v.maffione@gmail.com>
2017-12-04 15:56:49 +0100

    tests: integration tests: use pkill to stop uipcps

--- tests/run-integration-tests.sh
+++ tests/run-integration-tests.sh
@@ -55,7 +55,7 @@ for t in $(ls tests/integration/*); do
     sudo ${t}
     retcode="$?"
     sudo rlite-ctl reset || abort_cleanup
-    sudo kill $(cat /run/rlite/uipcps.pid) || abort_cleanup
+    sudo pkill rlite-uipcps || abort_cleanup
     for m in ${modules}; do
         sudo rmmod ${m} || abort_cleanup
     done
