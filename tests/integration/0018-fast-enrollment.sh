3f4811c11d64a83f83c21a8ed3bfa298d349b3fe
Vincenzo Maffione <v.maffione@gmail.com>
2017-12-21 16:44:35 +0100

    tests: integration: add fast enrollment test

---
+++ tests/integration/0018-fast-enrollment.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Create two normal IPCPs and enroll one to the other,
+# using a third IPCP as a N-1 DIF.
+# Use manual address allocation to speed up the test
+rlite-ctl ipcp-create x normal dd || exit 1
+rlite-ctl ipcp-create y normal dd || exit 1
+rlite-ctl ipcp-create z normal zz || exit 1
+rlite-ctl dif-policy-mod dd address-allocator manual || exit 1
+rlite-ctl ipcp-config x address 76 || exit 1
+rlite-ctl ipcp-config y address 77 || exit 1
+rlite-ctl ipcp-enroller-enable x || exit 1
+rlite-ctl ipcp-enroller-enable z || exit 1
+rlite-ctl ipcp-register x zz || exit 1
+rlite-ctl ipcp-register y zz || exit 1
+rlite-ctl ipcp-enroll y dd zz x
+# Reset to wait for all the flows to go away
+rlite-ctl reset
