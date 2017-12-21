81ef9eef53dfe199b958b1a0263acfca948311aa
Vincenzo Maffione <v.maffione@gmail.com>
2017-12-21 13:28:16 +0100

    tests: integration: test for default policies for enrollment and ra

--- tests/integration/0010-policy.sh
+++ tests/integration/0010-policy.sh
@@ -9,7 +9,9 @@ rlite-ctl dif-policy-mod dd address-allocator manual || exit 1
 rlite-ctl dif-policy-mod dd address-allocator distributed || exit 1
 rlite-ctl dif-policy-mod dd dft centralized-fault-tolerant || exit 1
 rlite-ctl dif-policy-mod dd dft fully-replicated || exit 1
+rlite-ctl dif-policy-mod dd enrollment default || exit 1
 rlite-ctl dif-policy-mod dd flow-allocator local || exit 1
+rlite-ctl dif-policy-mod dd resource-allocator default || exit 1
 rlite-ctl dif-policy-mod dd routing link-state || exit 1
 rlite-ctl dif-policy-mod dd routing link-state-lfa || exit 1
 
