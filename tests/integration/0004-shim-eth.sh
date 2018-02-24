bcbcace85bc3d9ed06bb3360f3dc4551e931b312
Vincenzo Maffione <v.maffione@gmail.com>
2018-02-24 09:32:24 +0100

    tests: propagate failure on veth operations

--- tests/integration/0004-shim-eth.sh
+++ tests/integration/0004-shim-eth.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
+function cleanup() {
+    local ret=0
+    pkill rina-echo-async
+    rlite-ctl reset || ret=1
+    ip link set rina.veth0 down || ret=1
+    ip link set rina.veth1 down || ret=1
+    ip link del rina.veth0 || ret=1
+    [ "$ret" != 0 ] && return 1 || return 0
+}
+
+function abort() {
+    cleanup
+    exit 1
+}
+
 # Create a veth pair
-ip link add rina.veth0 type veth peer name rina.veth1
-ip link set rina.veth0 up
-ip link set rina.veth1 up
+ip link add rina.veth0 type veth peer name rina.veth1 || exit 1
+ip link set rina.veth0 up || abort
+ip link set rina.veth1 up || abort
 
 # Create a shim-eth for each end of the pair. DIF should be
 # the same, but we'll cheat only for the purpose of testing.
-rlite-ctl ipcp-create s0 shim-eth d0 || exit 1
-rlite-ctl ipcp-create s1 shim-eth d1 || exit 1
-rlite-ctl ipcp-config s0 netdev rina.veth0 || exit 1
-rlite-ctl ipcp-config s1 netdev rina.veth1 || exit 1
+rlite-ctl ipcp-create s0 shim-eth d0 || abort
+rlite-ctl ipcp-create s1 shim-eth d1 || abort
+rlite-ctl ipcp-config s0 netdev rina.veth0 || abort
+rlite-ctl ipcp-config s1 netdev rina.veth1 || abort
 
 # Register an application on the first shim-eth
-rina-echo-async -lw -z rpinstance4 -d d0 || exit 1
+rina-echo-async -lw -z rpinstance4 -d d0 || abort
 # Run some clients on the other shim-eth
-rina-echo-async -z rpinstance4 -d d1 || exit 1
+rina-echo-async -z rpinstance4 -d d1 || abort
 
 # Cleanup
-pkill rina-echo-async
-rlite-ctl reset || exit 1
-ip link del rina.veth0 || exit 1
+cleanup
