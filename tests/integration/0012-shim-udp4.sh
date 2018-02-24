eff370404d02361edb1c6764c87dd644306ebb91
Vincenzo Maffione <v.maffione@gmail.com>
2018-02-24 09:32:24 +0100

    tests: propagate failure on veth operations

--- tests/integration/0012-shim-udp4.sh
+++ tests/integration/0012-shim-udp4.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 function cleanup() {
+    local ret=0
     pkill rina-echo-async
-    rlite-ctl reset || exit 1
-    ip link del rinau.veth0 || exit 1
+    rlite-ctl reset || ret=1
+    ip link set rinau.veth0 up || ret=1
+    ip link set rinau.veth1 up || ret=1
+    ip link del rinau.veth0 || ret=1
     cp /etc/hosts.save /etc/hosts
+    [ "$ret" != 0 ] && return 1 || return 0
 }
 
 function abort() {
@@ -13,11 +17,11 @@ function abort() {
 }
 
 # Create a veth pair
-ip link add rinau.veth0 type veth peer name rinau.veth1
-ip link set rinau.veth0 up
-ip link set rinau.veth1 up
-ip addr add 10.11.12.13/24 dev rinau.veth0
-ip addr add 10.11.12.14/24 dev rinau.veth1
+ip link add rinau.veth0 type veth peer name rinau.veth1 || exit 1
+ip link set rinau.veth0 up || abort
+ip link set rinau.veth1 up || abort
+ip addr add 10.11.12.13/24 dev rinau.veth0 || abort
+ip addr add 10.11.12.14/24 dev rinau.veth1 || abort
 cp /etc/hosts /etc/hosts.save
 echo "10.11.12.13 rpinstance5" > /etc/hosts
 
