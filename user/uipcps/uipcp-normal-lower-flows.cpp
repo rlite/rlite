bfaeef321a512883ab58ef2ecda34762b6f8ae1a
Vincenzo Maffione <v.maffione@gmail.com>
2018-05-11 05:57:01 +0200

    uipcps: lower-flows: add TODO item to investigate

--- user/uipcps/uipcp-normal-lower-flows.cpp
+++ user/uipcps/uipcp-normal-lower-flows.cpp
@@ -802,8 +802,12 @@ RoutingEngine::compute_fwd_table()
             }
 
             if (!neigh->second->has_flows()) {
-                UPE(uipcp, "N-1 flow towards neigh %s just disappeared\n",
-                    lfa.c_str());
+                /* There is a time window where this can happen. It happens
+                 * because of the update_local() call in the
+                 * neighbors_handler().
+                 * TODO Remove that call and fix the integration tests. */
+                UPW(uipcp, "No flow for next hop %s\n",
+                    neigh->second->ipcp_name.c_str());
                 continue;
             }
 
