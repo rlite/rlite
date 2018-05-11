095cee73d2ca836ccd4c5fe982530da0763211ca
Vincenzo Maffione <v.maffione@gmail.com>
2018-05-11 05:57:01 +0200

    test: integration: finalize LFA test

--- tests/integration/0028-resilient-routing.sh
+++ tests/integration/0028-resilient-routing.sh
@@ -18,6 +18,17 @@ abort() {
     exit 1
 }
 
+# First parameter: link name (e.g. "ab")
+# Second parameter: "up" or "down"
+link_set() {
+    local li=$1
+    local status=$2
+    left=${li:0:1}
+    right=${li:1:1}
+    ip netns exec $left ip link set veth.${li}l $status || abort
+    ip netns exec $right ip link set veth.${li}r $status || abort
+}
+
 # Create four namespaces, connected on the same LAN through a software
 # bridge and veth pairs.
 #
@@ -61,15 +72,33 @@ for li in ab ac ad; do
     right=${li:1:1}
     ip netns exec ${right} rlite-ctl ipcp-enroll ${right}.n normdif ${li}dif ${left}.n || abort
 done
-
 ip netns exec a rlite-ctl dif-routing-show normdif || abort
-
 for li in bd cd; do
     left=${li:0:1}
     right=${li:1:1}
     ip netns exec ${right} rlite-ctl ipcp-lower-flow-alloc ${right}.n normdif ${li}dif ${left}.n || abort
 done
-
 ip netns exec a rlite-ctl dif-routing-show normdif || abort
+
+# Run a server on D
+ip netns exec d rinaperf -lw -z rpinstd || abort
+# Check that A can connect to D
+ip netns exec a rinaperf -z rpinstd -i 0 -c 1 || abort
+# Now bring AD down
+link_set ad down
+# Check that A can still connect to D (through B)
+ip netns exec a rinaperf -z rpinstd -i 0 -c 1 || abort
+# Now bring AB down
+link_set ab down
+# Check that A can still connect to D (through C)
+ip netns exec a rinaperf -z rpinstd -i 0 -c 1 || abort
+# Now bring AB down
+link_set ac down
+# Check that A cannot connect to D
+ip netns exec a rinaperf -z rpinstd -i 0 -c 1 && abort
+# Bring AD back up
+link_set ad up
+# Check that A can connect to D
+ip netns exec a rinaperf -z rpinstd -i 0 -c 1 || abort
 
 cleanup
