0aa017bf449727f55cc25596555b2a532835b459
Vincenzo Maffione <v.maffione@gmail.com>
2018-05-11 05:57:01 +0200

    tests: improve policy-list test

--- tests/integration/0017-policy-list.sh
+++ tests/integration/0017-policy-list.sh
@@ -9,9 +9,17 @@ rlite-ctl dif-policy-list dd || exit 1
 rlite-ctl dif-policy-list dd addralloc | grep -q "\<distributed\>" || exit 1
 rlite-ctl dif-policy-mod dd addralloc static || exit 1
 rlite-ctl dif-policy-list dd addralloc | grep -q "\<static\>" || exit 1
+rlite-ctl dif-policy-mod dd addralloc centralized-fault-tolerant || exit 1
+rlite-ctl dif-policy-list dd addralloc | grep -q "\<centralized-fault-tolerant\>" || exit 1
+
+rlite-ctl dif-policy-list dd dft | grep -q "\<fully-replicated\>" || exit 1
+rlite-ctl dif-policy-mod dd dft centralized-fault-tolerant || exit 1
+rlite-ctl dif-policy-list dd dft | grep -q "\<centralized-fault-tolerant\>" || exit 1
+
 rlite-ctl dif-policy-list dd enrollment | grep -q "\<default\>"|| exit 1
 rlite-ctl dif-policy-list dd flowalloc | grep -q "\<local\>"|| exit 1
 rlite-ctl dif-policy-list dd resalloc | grep -q "\<default\>" || exit 1
+
 rlite-ctl dif-policy-list dd routing | grep -q "\<link-state\>" || exit 1
 rlite-ctl dif-policy-mod dd routing link-state-lfa || exit 1
 rlite-ctl dif-policy-list dd routing | grep -q "\<link-state-lfa\>" || exit 1
