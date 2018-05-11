18f68705c32679788eb5d1afd97ac8630ff480aa
Vincenzo Maffione <v.maffione@gmail.com>
2018-05-11 05:57:01 +0200

    tests: integration: add test with broadcast enrollment

---
+++ tests/integration/0025-broadcast-enrollment.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+cleanup() {
+    local ret=0
+    pkill rinaperf
+    ip netns exec green rlite-ctl reset || ret=1
+    ip netns exec green rlite-ctl terminate || ret=1
+    ip netns exec red rlite-ctl reset || ret=1
+    ip netns exec red rlite-ctl terminate || ret=1
+    rlite-ctl reset || ret=1
+    ip netns delete green || ret=1
+    ip netns delete red || ret=1
+    # veths are autodeleted once the namespace is deleted
+    [ "$ret" != 0 ] && return 1 || return 0
+}
+
+abort() {
+    cleanup
+    exit 1
+}
+
+# Create two namespaces, a veth pair, and assign each end of the pair
+# to a different namespace.
+ip link add veth.green type veth peer name veth.red || abort
+ip link set veth.red up || abort
+ip netns add green || abort
+ip netns add red || abort
+ip link set veth.green netns green || abort
+ip link set veth.red netns red || abort
+
+# Normal over shim eth setup in the green namespace
+ip netns exec green ip link set lo up || abort
+ip netns exec green rlite-uipcps -d || abort
+ip netns exec green ip link set veth.green up || abort
+ip netns exec green rlite-ctl ipcp-create green.eth shim-eth edif || abort
+ip netns exec green rlite-ctl ipcp-config green.eth netdev veth.green || abort
+ip netns exec green rlite-ctl ipcp-create green.n normal mydif || abort
+ip netns exec green rlite-ctl dif-policy-mod mydif addralloc static || abort
+ip netns exec green rlite-ctl ipcp-config green.n address 8172 || abort
+ip netns exec green rlite-ctl ipcp-enroller-enable green.n || abort
+ip netns exec green rlite-ctl ipcp-register green.n edif || abort
+ip netns exec green rinaperf -lw -z rpinst1 || abort
+
+# Normal over shim eth setup in the red namespace
+ip netns exec red ip link set lo up || abort
+ip netns exec red rlite-uipcps -d || abort
+ip netns exec red ip link set veth.red up || abort
+ip netns exec red rlite-ctl ipcp-create red.eth shim-eth edif || abort
+ip netns exec red rlite-ctl ipcp-config red.eth netdev veth.red || abort
+ip netns exec red rlite-ctl ipcp-create red.n normal mydif || abort
+ip netns exec red rlite-ctl dif-policy-mod mydif addralloc static || abort
+ip netns exec green rlite-ctl ipcp-config green.n address 2718 || abort
+ip netns exec red rlite-ctl ipcp-register red.n edif || abort
+ip netns exec red rlite-ctl ipcp-enroll red.n mydif edif || abort
+ip netns exec red rinaperf -z rpinst1 -p 1 -c 7 -i 20 || abort
+cleanup
