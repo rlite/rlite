bce5c624eb7bb669b54e003b46fcc9ac9332e95e
Vincenzo Maffione <v.maffione@gmail.com>
2018-11-11 12:47:44 +0100

    demo: enroll-chart: fix parsing and generate cumulative sums

--- demo/enroll-chart.py
+++ demo/enroll-chart.py
@@ -19,6 +19,9 @@ class EnrollEvent:
     def __repr__(self):
         return "%s at %02d:%02d:%02d" % (self.node, self.hr, self.mi, self.sec)
 
+    def timestamp(self):
+        return 3600*self.hr+60*self.mi+self.sec
+
 
 o = subprocess.check_output(['./greplog.sh', 'Enroller enabled'])
 o = str(o)
@@ -28,20 +31,47 @@ o = o.replace('\\n', '\n')
 o = o.split('\n')
 
 events = []
-i = 0
-while i < len(o) - 1:
-    m1 = re.search(r'log for node (\S+)', o[i])
-    m2 = re.search(r'(\d\d):(\d\d):(\d\d)', o[i+1])
+for line in o:
+    m1 = re.search(r'uipcp\.([^.]+)\.log', line)
+    m2 = re.search(r'(\d\d):(\d\d):(\d\d)', line)
     if m1 is not None and m2 is not None:
         node = m1.group(1)
         hr, mi, sec = int(m2.group(1)), int(m2.group(2)), int(m2.group(3))
         t = hr * 3600 + mi * 60 + sec
         events.append(EnrollEvent(node, hr, mi, sec))
-        i += 2
-    else:
-        i += 1
 
-events.sort(key = lambda x: (3600*x.hr+60*x.mi+x.sec))
+# Sort by time
+events.sort(key = lambda x: (x.timestamp()))
 
+if len(events) == 0:
+    # Nothing to do
+    quit(0)
+
+# Print the sorted events
 for e in events:
     print(e)
+
+first_t = events[0].timestamp()
+
+# Group by time, computing a cumulative count
+cumcounts = dict()
+last_time = -1
+count_partial = 0
+count_total = 0
+for i in range(len(events)):
+    count_partial += 1
+    t = events[i].timestamp()
+    if i+1 < len(events):
+        t_next = events[i+1].timestamp()
+    else:
+        t_next = -2  # invalid
+    if t_next != t:
+        # Finalize the last group
+        count_total += count_partial
+        cumcounts[t-first_t] = count_total
+        # Start a new group
+        count_partial = 0
+
+for t in sorted(cumcounts):
+    print("t=%04d: %4d enrolled" % (t, cumcounts[t]))
+    #print("%4d %4d" % (t, cumcounts[t]))
